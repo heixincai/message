f1a566010959f8bb57acd0c0ed55089b551a1e62
suyd <suyd@mingdutech.com>
2024-06-12 10:01:59 +0800

    【初始化】：第一次提交

---
+++ src/test/java/com/message/util/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
